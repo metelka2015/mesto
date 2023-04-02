96681ef6411c8c663b39d15c2b69613c7d6dd15e
Elena <metelka2015@gmail.com>
2023-04-03 04:02:15 +0500

    feat: добавить блок popup

---
+++ index.js
@@ -0,0 +1,30 @@
+const editButtonLink = document.querySelector('.profile__edit-button-link');
+const editButtonPopup = document.querySelector('.popup_type_profile');
+const editButtonPopupCloseButton = editButtonPopup.querySelector('.popup__close');
+const nameInput = editButtonPopup.querySelector('.popup__input_type_name');
+const jobInput = editButtonPopup.querySelector('.popup__input_type_job');
+const editButtonPopupSubmit = editButtonPopup.querySelector('.popup__submit');
+const editButtonPopupForm = editButtonPopup.querySelector('.popup__form');
+const profileName = document.querySelector('.profile__title');
+const profileJob = document.querySelector('.profile__subtitle')
+
+function handleClick() {
+  editButtonPopup.classList.add('popup_open');
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileJob.textContent;
+}
+
+editButtonLink.addEventListener('click', handleClick);
+
+editButtonPopupCloseButton.addEventListener('click', () => {
+  editButtonPopup.classList.remove('popup_open');
+});
+
+editButtonPopupForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+  profileName.textContent = nameInput.value;
+  profileJob.textContent = jobInput.value;
+  editButtonPopup.classList.remove('popup_open');
+});
+
+
