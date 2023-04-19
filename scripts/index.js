eeb61e5ed99ad6b87f29003131a2f8b50bed89d9
Elena <metelka2015@gmail.com>
2023-04-20 00:22:05 +0500

    feat: добавить форму добавления фотографий

--- scripts/index.js
+++ scripts/index.js
@@ -26,7 +26,7 @@ const initialCards = [
 ];
 
 const editButtonLink = document.querySelector('.profile__edit-button');
-const editButtonPopup = document.querySelector('.popup');
+const editButtonPopup = document.querySelector('.popup_type_profile');
 const editButtonPopupCloseButton = editButtonPopup.querySelector('.popup__close');
 const nameInput = editButtonPopup.querySelector('.popup__input_type_name');
 const jobInput = editButtonPopup.querySelector('.popup__input_type_job');
@@ -35,14 +35,34 @@ const editButtonPopupForm = editButtonPopup.querySelector('.popup__form');
 const profileName = document.querySelector('.profile__title');
 const profileJob = document.querySelector('.profile__subtitle');
 
+const addButtonLink = document.querySelector('.profile__add-button-link');
+const addButtonPopup = document.querySelector('.popup_type_place');
+const addButtonPopupClose = addButtonPopup.querySelector('.popup__close');
+const placeNameInput = addButtonPopup.querySelector('.popup__input_type_placename');
+const placeLinkInput = addButtonPopup.querySelector('.popup__input_type_placelink');
+const addButtonPopupSubmit = addButtonPopup.querySelector('.popup__submit');
+const addButtonPopupForm = addButtonPopup.querySelector('.popup__form');
+const placeName = document.querySelector('.profile__title');
+const placeLink = document.querySelector('.profile__subtitle');
+
 function handleClick() {
   editButtonPopup.classList.add('popup_opened');
   nameInput.value = profileName.textContent;
   jobInput.value = profileJob.textContent;
 }
 
+function handleClickAdd() {
+  addButtonPopup.classList.add('popup_opened');
+  placeName.value = placeName.textContent;
+  placeLink.value = placeLink.textContent;
+}
+
 function closePopup() {
   editButtonPopup.classList.remove('popup_opened');
+}
+
+function closePopupPlace() {
+  addButtonPopup.classList.remove('popup_opened');
 }
 
 function handleSubmit(event) {
@@ -52,10 +72,22 @@ function handleSubmit(event) {
   editButtonPopup.classList.remove('popup_opened');
 }
 
+function handleCreate(event) {
+  event.preventDefault();
+  placeName.textContent = placeName.value;
+  placeLink.textContent = placeLink.value;
+  addButtonPopup.classList.remove('popup_opened');
+}
+
 editButtonLink.addEventListener('click', handleClick);
 
 editButtonPopupCloseButton.addEventListener('click', closePopup);
 
 editButtonPopupForm.addEventListener('submit', handleSubmit);
 
+addButtonLink.addEventListener('click', handleClickAdd);
+
+addButtonPopupClose.addEventListener('click', closePopupPlace);
+
+addButtonPopupForm.addEventListener('submit', handleCreate);
 
