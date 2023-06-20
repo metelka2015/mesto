7eb4df4107e034f4b169bbdb96fb139e512b6ca1
Elena <metelka2015@gmail.com>
2023-06-21 02:12:33 +0500

    fix: переименовать функцию

--- src/pages/index.js
+++ src/pages/index.js
@@ -23,19 +23,21 @@ import Api from "../scripts/Api.js";
 import PopupDeleteCard from "../scripts/PopupDeleteCard.js";
 
 const api = new Api({
-  url: 'https://mesto.nomoreparties.co/v1/cohort-68',
+  url: "https://mesto.nomoreparties.co/v1/cohort-68",
   headers: {
-    authorization: '13586674-c353-49e6-be07-3fb91b47b641'
-  }
-
-})
+    authorization: "13586674-c353-49e6-be07-3fb91b47b641",
+  },
+});
 
 const profileValidator = new FormValidator(
   validationConfig,
   editButtonPopupForm
 );
 const cardValidator = new FormValidator(validationConfig, addButtonPopupForm);
-const avatarValidator = new FormValidator(validationConfig, document.querySelector('.popup__form_type_avatar'));
+const avatarValidator = new FormValidator(
+  validationConfig,
+  document.querySelector(".popup__form_type_avatar")
+);
 
 profileValidator.enableValidation();
 cardValidator.enableValidation();
@@ -44,14 +46,17 @@ avatarValidator.enableValidation();
 const popupImage = new PopupWithImage(".popup_type_image");
 popupImage.setEventListeners();
 
-
 const cardsElements = new Section(".elements__list", {
   renderer: (item) => {
     cardsElements.addItem(createCard(item));
   },
 });
 
-const userInfo = new UserInfo(".profile__title", ".profile__subtitle", ".profile__avatar");
+const userInfo = new UserInfo(
+  ".profile__title",
+  ".profile__subtitle",
+  ".profile__avatar"
+);
 
 let profileInfo;
 
@@ -67,10 +72,10 @@ Promise.all([api.getUserInfo(), api.getInitialCards()])
     console.log(error);
   });
 
-
 const popupFormProfile = new PopupWithForm(".popup_type_profile", (data) => {
   popupFormProfile.setButtonText(true);
-  api.setProfileInfo(data)
+  api
+    .setProfileInfo(data)
     .then((res) => {
       userInfo.setUserInfo(res);
       popupFormProfile.close();
@@ -91,7 +96,8 @@ const popupFormCard = new PopupWithForm(".popup_type_place", (cardData) => {
     link: placeLinkInput.value,
   };
   popupFormCard.setButtonText(true);
-  api.addNewCard(cardData)
+  api
+    .addNewCard(cardData)
     .then((item) => {
       const cardElement = createCard(item);
       popupFormCard.close();
@@ -105,21 +111,21 @@ const popupFormCard = new PopupWithForm(".popup_type_place", (cardData) => {
 });
 popupFormCard.setEventListeners();
 
-const popupDeleteCard = new PopupDeleteCard('.popup_type_delete');
+const popupDeleteCard = new PopupDeleteCard(".popup_type_delete");
 popupDeleteCard.setEventListeners();
 
 function createCard(cardData) {
-
   const card = new Card({
     data: cardData,
     profileInfo: profileInfo,
-    templateSelector: '.place-template',
+    templateSelector: ".place-template",
     handleCardClick: () => {
       popupImage.open(cardData.name, cardData.link);
     },
     handleDeleteClick: (cardId) => {
       popupDeleteCard.confirmDelete(() => {
-        api.deleteCard(cardId)
+        api
+          .deleteCard(cardId)
           .then(() => {
             card.handleDelete();
             popupDeleteCard.close();
@@ -131,7 +137,8 @@ function createCard(cardData) {
       popupDeleteCard.open();
     },
     addLike: () => {
-      api.addlike(card.getCurrentCard()._id)
+      api
+        .addLike(card.getCurrentCard()._id)
         .then((res) => {
           card.handleLike(res);
         })
@@ -140,31 +147,33 @@ function createCard(cardData) {
         });
     },
     removeLike: () => {
-      api.removelike(card.getCurrentCard()._id)
+      api
+        .removeLike(card.getCurrentCard()._id)
         .then((item) => {
           card.handleLike(item);
         })
         .catch((error) => {
           console.log(error);
         });
-    }
-    });
+    },
+  });
 
   const cardElement = card.generateCard();
   return cardElement;
 }
 
-const popupAvatar = new PopupWithForm('.popup_type_avatar', (data) => {
+const popupAvatar = new PopupWithForm(".popup_type_avatar", (data) => {
   popupAvatar.setButtonText(true);
-    api.editNewAvatar(data)
-      .then((res) => {
-        userInfo.setUserInfo(res);
-        popupAvatar.close();
-        console.log(res);
-      })
-      .catch((error) => console.log(error))
-      .finally(() => popupAvatar.setButtonText(false));
-  });
+  api
+    .editNewAvatar(data)
+    .then((res) => {
+      userInfo.setUserInfo(res);
+      popupAvatar.close();
+      console.log(res);
+    })
+    .catch((error) => console.log(error))
+    .finally(() => popupAvatar.setButtonText(false));
+});
 popupAvatar.setEventListeners();
 
 editButtonLink.addEventListener("click", () => {
@@ -178,6 +187,6 @@ addButtonLink.addEventListener("click", () => {
   popupFormCard.open();
 });
 
-editAvatarButton.addEventListener('click', () => {
+editAvatarButton.addEventListener("click", () => {
   popupAvatar.open();
-})
+});
--- src/scripts/Api.js
+++ src/scripts/Api.js
@@ -3,7 +3,6 @@ export default class Api {
     this._url = options.url;
     this._headers = options.headers;
     this._authorization = options.headers.authorization; //токен
-
   }
 
   _checkRes(res) {
@@ -15,100 +14,88 @@ export default class Api {
     return Promise.reject(`Ошибка: ${res.status}`);
   }
 
-
-
-  getUserInfo () {
+  getUserInfo() {
     return fetch(`${this._url}/users/me`, {
-      method: 'GET',
+      method: "GET",
       headers: {
-        authorization: this._authorization
+        authorization: this._authorization,
       },
-    })
-    .then(this._checkRes)
+    }).then(this._checkRes);
   }
 
   getInitialCards() {
     return fetch(`${this._url}/cards`, {
       headers: {
-        authorization: this._authorization
+        authorization: this._authorization,
       },
-    })
-    .then(this._checkRes)
+    }).then(this._checkRes);
   }
 
   setProfileInfo(data) {
     return fetch(`${this._url}/users/me`, {
-      method: 'PATCH',
+      method: "PATCH",
       headers: {
         authorization: this._authorization,
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({
         name: data.name,
-        about: data.about
-      })
-    })
-    .then(this._checkRes)
+        about: data.about,
+      }),
+    }).then(this._checkRes);
   }
 
   addNewCard(data) {
     return fetch(`${this._url}/cards`, {
-      method: 'POST',
+      method: "POST",
       headers: {
         authorization: this._authorization,
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({
         name: data.name,
-        link: data.link
-      })
-    })
-    .then(this._checkRes)
+        link: data.link,
+      }),
+    }).then(this._checkRes);
   }
 
   deleteCard(id) {
     return fetch(`${this._url}/cards/${id}`, {
-      method: 'DELETE',
-      headers: {
-        authorization: this._authorization
-      }
-    })
-    .then(this._checkRes)
-  }
-
-  addlike(id) {
-    return fetch(`${this._url}/cards/${id}/likes`, {
-      method: 'PUT',
-      headers: {
-        authorization: this._authorization
-      }
-    })
-    .then(this._checkRes)
-  }
-
-  removelike(id) {
-    return fetch(`${this._url}/cards/${id}/likes`, {
-      method: 'DELETE',
-      headers: {
-        authorization: this._authorization
-      }
-    })
-    .then(this._checkRes)
-  }
-
-  editNewAvatar(data){
-    return fetch(`${this._url}/users/me/avatar`, {
-      method: 'PATCH',
+      method: "DELETE",
       headers: {
         authorization: this._authorization,
-        'Content-Type': 'application/json'
+      },
+    }).then(this._checkRes);
+  }
+
+  addLike(id) {
+    return fetch(`${this._url}/cards/${id}/likes`, {
+      method: "PUT",
+      headers: {
+        authorization: this._authorization,
+      },
+    }).then(this._checkRes);
+  }
+
+  removeLike(id) {
+    return fetch(`${this._url}/cards/${id}/likes`, {
+      method: "DELETE",
+      headers: {
+        authorization: this._authorization,
+      },
+    }).then(this._checkRes);
+  }
+
+  editNewAvatar(data) {
+    return fetch(`${this._url}/users/me/avatar`, {
+      method: "PATCH",
+      headers: {
+        authorization: this._authorization,
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({
         avatar: data.avatar,
-      })
-    })
-    .then(this._checkRes)
+      }),
+    }).then(this._checkRes);
   }
 }
-
-
